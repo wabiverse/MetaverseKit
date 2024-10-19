2408830bf7f77814e3f5885438b101491d337c1d
furby™ <devs@wabi.foundation>
2024-10-19 15:29:47 -0600

    Import everything in tests.
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Tests/MetaverseKitTests/MetaverseKitTests.swift
+++ Tests/MetaverseKitTests/MetaverseKitTests.swift
@@ -29,12 +29,28 @@
  * ---------------------------------------------------------------- */
 
 import XCTest
+@testable import Draco
+@testable import Imath
 @testable import ImGui
+@testable import MaterialX
+@testable import OCIOBundle
+@testable import OpenColorIO
+@testable import OpenEXR
+@testable import OpenImageIO
+@testable import OpenSubdiv
+@testable import OpenTime
+@testable import OpenTimelineIO
+@testable import OpenVDB
+@testable import Ptex
+#if canImport(Python) && (os(macOS) || os(visionOS) || os(iOS) || os(tvOS) || os(watchOS))
+  @testable import PyBundle
+  @testable import Python
+#endif // canImport(Python) && os(macOS)
 
 class MetaverseKitTests: XCTestCase
 {
   func testMetaverse() throws
   {
-    print("TODO: Write tests.")
+    print("it works!")
   }
 }
