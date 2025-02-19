6e909df14486cb002ba778efadbf345917854da8
furby™ <devs@wabi.foundation>
2025-02-19 05:32:10 +0000

    fix eigen crashing AST on linux (fedora).
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Sources/Eigen/Eigen.cpp
+++ Sources/Eigen/Eigen.cpp
@@ -1,1 +1,6 @@
-#include <Eigen/Eigen.h>
+#if !defined(__linux__)
+// for some reason, including the top level Eigen include
+// on Linux might crash the swift AST parser, so we don't
+// include it here.
+# include <Eigen/Eigen.h>
+#endif
